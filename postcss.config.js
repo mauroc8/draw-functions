52eea8efaebea360cef76f4b0321237a1f9f7e9a
Mauro Cano <maurocanobrusa@gmail.com>
2020-08-08 19:53:08 -0300

    Disabled PurgeCSS because it strips off Tailwind's base and component styles!

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 // This file configures PostCSS to purge the unused Tailwind classes.
 
-const tailwind = require("tailwindcss");
+const plugins = [require("autoprefixer"), require("tailwindcss")];
 
-const plugins = [tailwind];
+/*
+
+NOTE: I could not prevent purgeCss to purge my base and component classes.
+I'll just disable it because there doesn't seem to be an alternative, except not using
+Parcel in the build process.
 
 const is_dev = process.env.NODE_ENV === "development";
 
@@ -20,5 +24,6 @@ if (!is_dev) {
 		})
 	);
 }
+*/
 
 module.exports = { plugins };
