a5f1a3f7171441a968a9a7d70f6a855ea32e88b5
Mauro Cano <maurocanobrusa@gmail.com>
2020-08-08 19:19:21 -0300

    PostCSS config to purge unused Tailwind classes!

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,24 @@
+// This file configures PostCSS to purge the unused Tailwind classes.
 
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require("tailwindcss");
 
-const plugins = [ tailwind ]
+const plugins = [tailwind];
 
-const is_dev = process.env.NODE_ENV === 'development'
+const is_dev = process.env.NODE_ENV === "development";
 
-if (!is_dev)
-{
+if (!is_dev) {
+	const purgecss = require("@fullhuman/postcss-purgecss");
 
+	const extractor = function (content) {
+		return content.match(/[A-z0-9\-:\/]+/g) || [];
+	};
+
+	plugins.push(
+		purgecss({
+			content: ["src/*.elm"],
+			defaultExtractor: extractor,
+		})
+	);
 }
 
-module.exports = { plugins }
+module.exports = { plugins };
